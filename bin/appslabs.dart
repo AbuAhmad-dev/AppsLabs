e0aa2cd1247f4e1bbe037fe08eae9a6296b1d46c
AbuAhmad-dev <basebata@gmail.com>
2025-12-11 04:37:31 +0300

    first commit

---
+++ bin/appslabs.dart
@@ -0,0 +1,5 @@
+import 'package:appslabs/appslabs.dart' as appslabs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${appslabs.calculate()}!');
+}
---
+++ test/appslabs_test.dart
@@ -0,0 +1,8 @@
+import 'package:appslabs/appslabs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
