ee2e89990a02178b733d17f770358fbf8a6b0c2c
AbuAhmad-dev <basebata@gmail.com>
2025-12-12 04:29:08 +0300

    task 6

--- lib/appslabs.dart
+++ lib/appslabs.dart
@@ -1,4 +1,23 @@
+void main () {
+Calculator calculator = Calculator();
+calculator.adition = 6+5;
+calculator.subtract = 6-5;
+calculator.multiply = 6*5;
+calculator.divide = 6/5 ;
+calculator.printResult();
+} 
 
+class Calculator {
+    double? adition;
+    double? subtract;
+    double? multiply;
+    double? divide ;
 
-
+    dynamic printResult () {
+        print(adition);
+        print(subtract);
+        print(multiply);
+        print(divide) ;
+    }
+}
 
